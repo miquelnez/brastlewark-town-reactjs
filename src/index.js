69c4060a29e840828a46ed9e719ddb33ab960967
rcastro <rcastro@mypshera.com>
2020-10-03 17:41:34 +0200

    feat: Initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'fontsource-roboto/900-normal.css';
 
 ReactDOM.render(
   <React.StrictMode>
